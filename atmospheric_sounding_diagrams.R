e240a712514725350c06e071f46a00410c748dae
rodrigolustosa <rodrigo.lustosa@usp.br>
2022-05-18 11:42:01 -0300

    initialize main script

---
+++ atmospheric_sounding_diagrams.R
@@ -0,0 +1,11 @@
+# This code reads and plots atmospheric sounding diagrams
+# Code developed by Rodrigo Lustosa, Wed May 18 2022
+
+# packages
+library(tidyverse)
+library(lubridate)
+
+# directory and file names
+dir_input  <- "input"
+dir_output <- "output"
+file_sounding <- "sounding.txt"
